d3f98594823d942b5b0597d35573e3f474e75e3c
Kavindu Ushan <kavindu.ushan@lassana.com>
2026-03-19 14:05:09 +0530

    project initialized

---
+++ src/main/java/com/ushan/lady_shoe_mart/LadyShoeMartApplication.java
@@ -0,0 +1,13 @@
+package com.ushan.lady_shoe_mart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LadyShoeMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LadyShoeMartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ushan/lady_shoe_mart/LadyShoeMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ushan.lady_shoe_mart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LadyShoeMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
