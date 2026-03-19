aead9b159507b4ce1abc595ecac11438d4d9f92e
Kavindu Ushan <kavindu.ushan@lassana.com>
2026-03-19 15:31:09 +0530

    feat: create AbstractEntity and AbstractModel

---
+++ src/main/java/com/ushan/lady_shoe_mart/common/util/AbstractEntity.java
@@ -0,0 +1,35 @@
+package com.ushan.lady_shoe_mart.common.util;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class AbstractEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractEntity that = (AbstractEntity) o;
+        return id != null && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+
+}
---
+++ src/main/java/com/ushan/lady_shoe_mart/common/util/AbstractModel.java
@@ -0,0 +1,10 @@
+package com.ushan.lady_shoe_mart.common.util;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public abstract class AbstractModel {
+    private Long id;
+}
