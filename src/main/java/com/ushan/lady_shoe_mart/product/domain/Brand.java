2d8194ac1a24c30d59df54179153314702cbc7db
Kavindu Ushan <kavindu.ushan@lassana.com>
2026-03-24 17:55:41 +0530

    fix: add product list

--- src/main/java/com/ushan/lady_shoe_mart/product/domain/Brand.java
+++ src/main/java/com/ushan/lady_shoe_mart/product/domain/Brand.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Date;
+import java.util.List;
 
 @Getter
 @Setter
@@ -17,4 +18,5 @@ public class Brand extends AbstractModel {
     private Boolean isActive;
     private Date dateCreated;
     private Date dateUpdated;
+    private List<Product> productList;
 }
