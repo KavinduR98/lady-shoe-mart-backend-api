4738ff192fe7bb706afd2ca6ca7cd7fa071a8d26
Kavindu Ushan <kavindu.ushan@lassana.com>
2026-03-24 17:56:43 +0530

    fix: add image related constants

--- src/main/java/com/ushan/lady_shoe_mart/common/util/ShoeMartConstant.java
+++ src/main/java/com/ushan/lady_shoe_mart/common/util/ShoeMartConstant.java
@@ -3,4 +3,8 @@ package com.ushan.lady_shoe_mart.common.util;
 public class ShoeMartConstant {
 
     public static final String IMAGE_FOLDER_BRAND = "brand_image";
+    public static final String IMAGE_PREFIX_BRAND = "brand";
+    public static final String[] ALLOWED_IMAGE_EXTENSIONS = {"jpg", "jpeg", "png", "gif", "webp"};
+    public static final long MAX_IMAGE_SIZE = 5 * 1024 * 1024; // 5MB
+    public static final String[] ALLOWED_IMAGE_MIME_TYPES = {"image/jpeg", "image/png", "image/gif", "image/webp"};
 }
