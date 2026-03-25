b998c59110b42fcc59600723b4ea67faa44e8245
Kavindu Ushan <kavindu.ushan@lassana.com>
2026-03-25 10:11:02 +0530

    feat: create get-all-brand api

--- src/main/java/com/ushan/lady_shoe_mart/product/service/IBrandService.java
+++ src/main/java/com/ushan/lady_shoe_mart/product/service/IBrandService.java
@@ -4,7 +4,11 @@ import com.ushan.lady_shoe_mart.common.util.ApiResponse;
 import com.ushan.lady_shoe_mart.product.domain.Brand;
 import com.ushan.lady_shoe_mart.product.domain.request.BrandRequest;
 
+import java.util.List;
+
 public interface IBrandService {
 
     ApiResponse<Brand> save(BrandRequest brand);
+
+    List<Brand> findAllBrand();
 }
