f379120a023d11d9cedb8a4b66673d0722c30c19
Kavindu Ushan <kavindu.ushan@lassana.com>
2026-03-27 10:23:21 +0530

    feat: create api for upload category image

--- src/main/java/com/ushan/lady_shoe_mart/admin/service/ICategoryService.java
+++ src/main/java/com/ushan/lady_shoe_mart/admin/service/ICategoryService.java
@@ -2,6 +2,7 @@ package com.ushan.lady_shoe_mart.admin.service;
 
 import com.ushan.lady_shoe_mart.admin.domain.Category;
 import com.ushan.lady_shoe_mart.common.util.ApiResponse;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -10,4 +11,6 @@ public interface ICategoryService {
     ApiResponse<Category> save(Category category);
 
     List<Category> findAllCategory();
+
+    ApiResponse<String> categoryImageUpload(MultipartFile file, Long categoryId);
 }
